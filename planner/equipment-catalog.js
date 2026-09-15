b7f25a1ef8b6e477f8a6763cc2f902c53e8f0910
Stagepulse.hatay <ibrahimkavasoglu7@gmail.com>
2026-09-16 00:53:57 +0300

    Kütüphane görsellerini sadeleştir ve gerçek ekipman yerleşimini düzelt

--- planner/equipment-catalog.js
+++ planner/equipment-catalog.js
@@ -1,6 +1,8 @@
-window.StagePulseEquipmentCatalog={version:2,items:[
-{cat:'instrument',name:'Shure SM58',img:'https://toppng.com/uploads/preview/sm58-shure-microphone-11549346284lrrlqoh7xg.png',w:.08,h:.32,source:'TopPNG',license:'personal-use'},
-{cat:'instrument',name:'Shure SM57',img:'https://w7.pngwing.com/pngs/884/383/png-transparent-microphone-shure-sm57-bass-drums-akg-acoustics-microphone-angle-electronics-microphone.png',w:.08,h:.30,source:'PNGWing',license:'non-commercial'},
-{cat:'pa',name:'Shure PSM300',img:'https://w7.pngwing.com/pngs/441/68/png-transparent-wireless-microphone-shure-p3tra215cl-psm300-wireless-stereo-personal-monitor-system-headphones-microphone-electronics-microphone-transmitter.png',w:.55,h:.30,source:'PNGWing',license:'non-commercial'},
-{cat:'console',name:'Midas M32',img:'https://e7.pngegg.com/pngimages/561/515/png-clipart-microphone-audio-mixers-digital-mixing-console-midas-consoles-mixer-electronics-microphone.png',w:1.55,h:.85,source:'PNGEgg',license:'non-commercial'}
+window.StagePulseEquipmentCatalog={version:3,items:[
+{cat:'console',name:'Midas M32',img:'https://e7.pngegg.com/pngimages/561/515/png-clipart-microphone-audio-mixers-digital-mixing-console-midas-consoles-mixer-electronics-microphone.png',w:1.55,h:.85},
+{cat:'instrument',name:'Bateri',img:'https://commons.wikimedia.org/wiki/Special:Redirect/file/Drum_set.png',w:2.10,h:1.55},
+{cat:'instrument',name:'Shure SM58',img:'https://www.vhv.rs/dpng/d/442-4428709_microphone-png-image-shure-sm58-transparent-png.png',w:.08,h:.32},
+{cat:'instrument',name:'Shure SM57',img:'https://commons.wikimedia.org/wiki/Special:Redirect/file/Shure_SM57.png',w:.08,h:.30},
+{cat:'pa',name:'Shure PSM300',img:'https://www.pngkey.com/png/detail/608-6088655_shure-psm-300-twin-pack-band-g20-aed.png',w:.55,h:.30},
+{cat:'pa',name:'Behringer P2',img:'https://casamusicalstrauss.com.pe/cdn/shop/products/behringer-p2_1024x1024.jpg',w:.10,h:.16}
 ]};
