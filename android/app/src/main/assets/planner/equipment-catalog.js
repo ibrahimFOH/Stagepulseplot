e4a772594cfef1ea97f278c03e0df8a5e82a6ceb
Stagepulse.hatay <ibrahimkavasoglu7@gmail.com>
2026-09-16 00:44:03 +0300

    Android plannerı yeni ekipman kütüphanesiyle senkronla

---
+++ android/app/src/main/assets/planner/equipment-catalog.js
@@ -0,0 +1,6 @@
+window.StagePulseEquipmentCatalog={version:2,items:[
+{cat:'instrument',name:'Shure SM58',img:'https://toppng.com/uploads/preview/sm58-shure-microphone-11549346284lrrlqoh7xg.png',w:.08,h:.32,source:'TopPNG',license:'personal-use'},
+{cat:'instrument',name:'Shure SM57',img:'https://w7.pngwing.com/pngs/884/383/png-transparent-microphone-shure-sm57-bass-drums-akg-acoustics-microphone-angle-electronics-microphone.png',w:.08,h:.30,source:'PNGWing',license:'non-commercial'},
+{cat:'pa',name:'Shure PSM300',img:'https://w7.pngwing.com/pngs/441/68/png-transparent-wireless-microphone-shure-p3tra215cl-psm300-wireless-stereo-personal-monitor-system-headphones-microphone-electronics-microphone-transmitter.png',w:.55,h:.30,source:'PNGWing',license:'non-commercial'},
+{cat:'console',name:'Midas M32',img:'https://e7.pngegg.com/pngimages/561/515/png-clipart-microphone-audio-mixers-digital-mixing-console-midas-consoles-mixer-electronics-microphone.png',w:1.55,h:.85,source:'PNGEgg',license:'non-commercial'}
+]};
