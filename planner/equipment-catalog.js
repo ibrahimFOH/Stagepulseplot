5ab4081165b0ecd30f8a0d39fb42cdaa1f565ed0
Stagepulse.hatay <ibrahimkavasoglu7@gmail.com>
2026-09-16 01:01:17 +0300

    Ekipman görsellerini şeffaf ürün görselleriyle düzelt

--- planner/equipment-catalog.js
+++ planner/equipment-catalog.js
@@ -1,8 +1,8 @@
-window.StagePulseEquipmentCatalog={version:3,items:[
-{cat:'console',name:'Midas M32',img:'https://e7.pngegg.com/pngimages/561/515/png-clipart-microphone-audio-mixers-digital-mixing-console-midas-consoles-mixer-electronics-microphone.png',w:1.55,h:.85},
+window.StagePulseEquipmentCatalog={version:4,items:[
+{cat:'console',name:'Midas M32',img:'https://cdn-media.empowertribe.com/2ec241f460b6433b93fbf6a39abf319a/Image_MI_0603-ADX_M32_Front_XL.png',w:1.55,h:.85},
 {cat:'instrument',name:'Bateri',img:'https://commons.wikimedia.org/wiki/Special:Redirect/file/Drum_set.png',w:2.10,h:1.55},
 {cat:'instrument',name:'Shure SM58',img:'https://www.vhv.rs/dpng/d/442-4428709_microphone-png-image-shure-sm58-transparent-png.png',w:.08,h:.32},
 {cat:'instrument',name:'Shure SM57',img:'https://commons.wikimedia.org/wiki/Special:Redirect/file/Shure_SM57.png',w:.08,h:.30},
 {cat:'pa',name:'Shure PSM300',img:'https://www.pngkey.com/png/detail/608-6088655_shure-psm-300-twin-pack-band-g20-aed.png',w:.55,h:.30},
-{cat:'pa',name:'Behringer P2',img:'https://casamusicalstrauss.com.pe/cdn/shop/products/behringer-p2_1024x1024.jpg',w:.10,h:.16}
+{cat:'pa',name:'Behringer P2',img:'https://cdn-media.empowertribe.com/242b61f813674fe6ac558c7015ed4cf0/Image_BE_0609-AAJ_P2_Front_XL.png',w:.10,h:.16}
 ]};
